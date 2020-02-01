e31f1dc23a81fada330e7f37c8af4c23d3daea69
Thomas Nix <tnix2@uncc.edu>
2020-02-01 11:27:27 -0500

    Set up routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { AboutComponent } from './pages/about/about.component';
 import { MediaComponent } from './pages/media/media.component';
 import { BookingComponent } from './pages/booking/booking.component';
 import { UpcomingShowsComponent } from './pages/upcoming-shows/upcoming-shows.component';
+import { RouterModule } from '@angular/router';
+
+const routes = [
+    {path: '', redirectTo: '/about', pathMatch: 'full'},
+    {path: 'about', component: AboutComponent},
+    {path: 'media', component: MediaComponent},
+    {path: 'booking', component: BookingComponent},
+    {path: 'upcoming-shows', component: UpcomingShowsComponent},
+    {path: '**', component: AboutComponent},
+];
 
 @NgModule({
   declarations: [
@@ -18,7 +27,8 @@ import { UpcomingShowsComponent } from './pages/upcoming-shows/upcoming-shows.co
     UpcomingShowsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
